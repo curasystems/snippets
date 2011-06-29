42f322c2d027ed2d2890d5eec8d0f57665ce86fb
Matthias Goetzke <m.goetzke@curasystems.de>
2011-06-29 17:58:35 +0200

    added themes

--- sublime/install.cmd
+++ sublime/install.cmd
@@ -1,1 +1,2 @@
-for /D %%f in (*.*) do copy %%f\*.sublime-snippet "%APPDATA%\Sublime Text 2\Packages\%%f"
+for /D %%f in (*.*) do copy "%%f\*.sublime-snippet" "%APPDATA%\Sublime Text 2\Packages\%%f"
+for /D %%f in (*.*) do copy "%%f\*.tmTheme" "%APPDATA%\Sublime Text 2\Packages\%%f"
