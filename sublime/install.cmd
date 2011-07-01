05cc64b9a9e6dc32eaeac1aae6d6ba690f8bb424
Matthias Goetzke <m.goetzke@curasystems.de>
2011-07-01 17:13:55 +0200

    delete old snippet on install

--- sublime/install.cmd
+++ sublime/install.cmd
@@ -1,2 +1,4 @@
+del "%APPDATA%\Sublime Text 2\Packages\Javascript\constructor.sublime-snippet"
+
 for /D %%f in (*.*) do copy "%%f\*.sublime-snippet" "%APPDATA%\Sublime Text 2\Packages\%%f"
 for /D %%f in (*.*) do copy "%%f\*.tmTheme" "%APPDATA%\Sublime Text 2\Packages\%%f"
